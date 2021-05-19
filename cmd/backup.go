d8f4f590e5224385e3b1bf98c00f36e8afff46c4
Doug Hatcher <doug.hatcher@blueacornici.com>
2021-05-18 23:48:05 -0400

    uploads to s3, with create

--- cmd/backup.go
+++ cmd/backup.go
@@ -22,10 +22,16 @@ THE SOFTWARE.
 package cmd
 
 import (
+	"fmt"
 	"os"
 	"strings"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var backupCmd = createBackupCmd()
@@ -49,13 +55,85 @@ func init() {
 
 	rootCmd.AddCommand(backupCmd)
 	testMode = strings.HasSuffix(os.Args[0], ".test")
+
+	viper.SetDefault("machine-s3-bucket", "mach-docker-machine-certificates")
+	viper.SetDefault("machine-s3-region", "us-east-1")
+
+	backupCmd.Flags().BoolP("create", "c", false, "create the bucket before attempting backup")
+
 }
 
 func runBackup(cmd *cobra.Command, args []string) error {
 
-	if len(args) < 1 {
+	createFirst, _ := cmd.Flags().GetBool("create")
 
+	if createFirst {
+		createBucket()
+	}
+
+	if len(args) == 1 {
+		uploadFileToBucket(args[0])
 	}
 
 	return nil
+}
+
+func createBucket() {
+	bucket := viper.GetString("machine-s3-bucket")
+
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(viper.GetString("machine-s3-region"))},
+	)
+
+	// Create S3 service client
+	svc := s3.New(sess)
+
+	_, err = svc.CreateBucket(&s3.CreateBucketInput{
+		Bucket: aws.String(bucket),
+	})
+	if err != nil {
+		exitErrorf("Unable to create bucket %q, %v", bucket, err)
+	}
+
+	// Wait until bucket is created before finishing
+	fmt.Printf("Waiting for bucket %q to be created...\n", bucket)
+
+	err = svc.WaitUntilBucketExists(&s3.HeadBucketInput{
+		Bucket: aws.String(bucket),
+	})
+
+}
+
+func uploadFileToBucket(filename string) {
+	bucket := viper.GetString("machine-s3-bucket")
+
+	file, err := os.Open(filename)
+	if err != nil {
+		exitErrorf("Unable to open file %q, %v", err)
+	}
+
+	defer file.Close()
+
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(viper.GetString("machine-s3-region"))},
+	)
+
+	uploader := s3manager.NewUploader(sess)
+
+	_, err = uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(filename),
+		Body:   file,
+	})
+	if err != nil {
+		// Print the error and exit.
+		exitErrorf("Unable to upload %q to %q, %v", filename, bucket, err)
+	}
+
+	fmt.Printf("Successfully uploaded %q to %q\n", filename, bucket)
+}
+
+func exitErrorf(msg string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, msg+"\n", args...)
+	os.Exit(1)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/superterran/mach
 go 1.16
 
 require (
+	github.com/aws/aws-sdk-go v1.38.42 // indirect
 	github.com/containerd/containerd v1.5.1 // indirect
 	github.com/docker/docker v20.10.6+incompatible
 	github.com/docker/go-connections v0.4.0 // indirect
